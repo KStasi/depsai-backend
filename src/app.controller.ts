aaecd6aad91ca8a7817abc8fed9f23bee10c8ad8
Digberi <orkepuma@gmail.com>
2024-02-18 15:35:55 +0200

    *

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -26,7 +26,7 @@ export class AppController {
     link: string;
   }> {
     return this.proxyService.createChild({
-      package: 'd7f78a202dd00ce8d979db5d1a31388d408d989f9fd2cc8596c43517',
+      package: '817352f3caceb659927e5d1b0cf0e58e60b3e988922e19431b46246b', //'d7f78a202dd00ce8d979db5d1a31388d408d989f9fd2cc8596c43517',
       command: 'npm run start',
       port: '7878',
     });
